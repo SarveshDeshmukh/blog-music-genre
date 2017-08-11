283b66fe41e5c4cf3ddfeead49e746638600953b
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-08-11 15:39:11 +0000

    add Edit and Update route

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ var express    = require("express"),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose");
     app        = express();
+    methodOverride = require("method-override");
  
  //APP CONFIG   
 mongoose.connect("mongodb://localhost/musicGenreBlog",{useMongoClient : true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     video: String,
+    videoEmbedLink: String,
     body : String,
     created :{
                 type: Date, 
@@ -53,8 +56,7 @@ app.get("/genres/new", function(req, res){
 app.post("/genres", function(req, res){
     
     var urlToEmbed = getId( req.body.genre.video);
-    console.log(urlToEmbed);
-    req.body.genre.video = urlToEmbed;
+    req.body.genre.videoEmbedLink = urlToEmbed;
     //Create genre
     Blog.create(req.body.genre, function(err, newGenre){
        if(err){
@@ -64,8 +66,6 @@ app.post("/genres", function(req, res){
            res.redirect("/genres");
        }
     })
-    //Redirect
-    
 });
 
 //SHOW ROUTE
@@ -82,7 +82,32 @@ app.get("/genres/:id", function(req, res){
     })
 });
 
+//EDIT ROUTE
+app.get("/genres/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundGenre){
+        if(err){
+            res.redirect("/genres");
+        }
+        else{
+            res.render("edit", {foundGenre: foundGenre});   
+        }
+    });
+});
 
+//PUT ROUTE 
+
+app.put("/genres/:id", function(req, res){
+    console.log("here in put")
+    Blog.findByIdAndUpdate(req.params.id, req.body.genre, function(err, updatedBlog){
+        if(err){
+            res.redirect("/genres");
+        }else{
+            res.redirect("/genres/"+req.params.id);
+        }
+    });
+});
+        
+//Function to get the link for youtube iframe embed.
 function getId(url) {
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     var match = url.match(regExp);
